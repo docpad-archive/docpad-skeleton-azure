06a9fb2d65c6c9f93c76d6f302ff62c4ef87c330
Benjamin Lupton <b@lupton.cc>
2013-01-25 22:54:09 +1100

    test

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-var http = require('http')
+console.log('VERSIONS:',process.versions);
+console.log('ENV:',process.env);
+
+var http = require('http');
 var port = process.env.PORT || 1337;
+console.log('PORT:',port);
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
