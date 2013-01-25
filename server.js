33fe0b1fc1c3815a5a0fd69090d42fba45a912cf
Benjamin Lupton <b@lupton.cc>
2013-01-25 22:15:54 +1100

    versions

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+console.log('VERSIONS:',process.versions);
 console.log('ENV:',process.env);
+
 var d = require('domain').create();
 d.on('error', function(er) {
   console.error('Caught error!', er);
