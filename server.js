b467d112c1de6b8b341e7f41ffb25a6d3d4e9215
Benjamin Lupton <b@lupton.cc>
2013-01-25 22:12:57 +1100

    added domain

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 console.log('ENV:',process.env);
-module.exports = require(__dirname+'/node_modules/docpad/out/bin/docpad-server');
+var d = require('domain').create();
+d.on('error', function(er) {
+  console.error('Caught error!', er);
+});
+d.run(function() {
+	module.exports = require(__dirname+'/node_modules/docpad/out/bin/docpad-server');
+});
