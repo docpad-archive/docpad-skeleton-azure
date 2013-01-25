6a05c2d28017166293806fb6c43dab74c8ba03ba
Benjamin Lupton <b@lupton.cc>
2013-01-25 23:08:06 +1100

    docpad again

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 console.log('VERSIONS:',process.versions);
 console.log('ENV:',process.env);
 
-var http = require('http');
 var port = process.env.PORT || 1337;
 console.log('PORT:',port);
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+
+var d = require('domain').create();
+d.on('error', function(er) {
+  console.error('Caught error!', er);
+});
+d.run(function() {
+	module.exports = require(__dirname+'/node_modules/docpad/out/bin/docpad-server');
+});
